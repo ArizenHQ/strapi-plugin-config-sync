c348bd687bdebb4f1825f58e4b2a98eaba7862b9
Alexandre Ouicher <aouicher@cobalt.fr>
2024-02-07 15:21:30 +0100

    Add configuration module to controllers and services

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const config = require('./config');
+const configuration = require('./configuration');
 
 module.exports = {
   config: config,
+  configuration: configuration,
 };
--- server/services/index.js
+++ server/services/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const main = require('./main');
+const configuration = require('./configuration');
 
 module.exports = {
   main,
+  configuration,
 };
