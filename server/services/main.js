80a00f26a95084bb9b85a19c96d0e7c2267f1a8f
Alexandre Ouicher <aouicher@cobalt.fr>
2024-02-10 05:27:28 +0100

    Add console.log statement to check GITHUB_TOKEN

--- server/services/main.js
+++ server/services/main.js
@@ -343,7 +343,7 @@ module.exports = () => ({
         };
 
         try {
-
+          console.log("createPR ~ process.env.GITHUB_TOKEN:", process.env.GITHUB_TOKEN);
           const response = await fetch(`https://api.github.com/repos/${orga}/${repo}/pulls`, {
             method: 'POST',
             headers: {
