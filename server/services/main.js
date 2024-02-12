93fca50a27bb6b2f9b739843f574ecca6a8bcf5a
Alexandre Ouicher <aouicher@cobalt.fr>
2024-02-12 11:06:50 +0100

    Update git command to use 'git pull' instead of 'git fetch'

--- server/services/main.js
+++ server/services/main.js
@@ -314,7 +314,7 @@ module.exports = () => ({
         `git config user.email "${userEmail}"`,
         `git config user.name "${userName}"`,
         `git config --global --add safe.directory ${process.env.PWD}`,
-        `git fetch ${urlRepo} master`,
+        `git pull ${urlRepo} master`,
         `git -C "." checkout -b ${branchName}`,
         `git -C "." add .`,
         `git -C "." diff --cached --exit-code || git -C "." commit -m "${commitMessage}"`,
