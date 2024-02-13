5c42a4b7a6a2c21c169395193fa701438b6f65c6
Alexandre Ouicher <aouicher@cobalt.fr>
2024-02-13 17:11:02 +0100

    Update console log in main.js

--- server/services/main.js
+++ server/services/main.js
@@ -325,7 +325,7 @@ module.exports = () => ({
         await previousPromise;
         return new Promise((resolve, reject) => {
           exec(command, (error, stdout, stderr) => {
-            console.log("exec ~ stdout:", stdout, stderr);
+            console.log("exec ~ stdout:", command, stdout, stderr);
             if (error) {
               console.log(`exec error: ${error}`);
               return reject(error);
