5907acfaa8fc974444c1029e321bb6461210fab7
Hani Benzouache <hani.benzouache@coinhouse.com>
2026-03-24 13:42:47 +0100

    [RUN] fix redux-thunk v3 import - use named export thunk instead of default

--- admin/src/helpers/configureStore.js
+++ admin/src/helpers/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { thunk as thunkMiddleware } from 'redux-thunk';
 import { Map } from 'immutable';
 
 import rootReducer from '../state/reducers';
